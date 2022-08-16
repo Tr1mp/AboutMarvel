b7b418853df4dce50d00f4c906995360b85eeeb1
Tr1mp <@Tr1mp>
2022-08-16 20:21:23 +0300

    develop. Add PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,10 +1,12 @@
 import { Component } from "react";
+import PropTypes from 'prop-types';
 
-import "./charInfo.scss";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
+
+import "./charInfo.scss";
 
 class CharInfo extends Component {
     state = {
@@ -125,6 +127,10 @@ const View = ({char}) => {
                 </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from "../../services/MarvelService";
 import Spinner from '../spinner/Spinner';
@@ -109,6 +110,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
